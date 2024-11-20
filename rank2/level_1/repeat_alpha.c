c8b32657c6a2b2359e6763959949a222f92f8bc0
curtyraissa <raissacurty@gmail.com>
2024-11-20 16:45:07 +0000

    feat: add repeat_alpha function

--- rank2/level_1/repeat_alpha.c
+++ rank2/level_1/repeat_alpha.c
@@ -29,3 +29,33 @@ $>./repeat_alpha "" | cat -e
 $
 $>
 */
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i;
+    int c;
+
+    i = 0;
+    if (ac == 2)
+    {
+    while (av[1][i])
+    {
+        if (av[1][i] >= 'a' && av[1][i] <= 'z')
+            c = av[1][i] - 'a' + 1;
+        else if (av[1][i] >= 'A' && av[1][i] <='Z')
+            c = av[1][i] - 'A' + 1;
+        else
+            c = 1;
+        while (c > 0)
+        {
+            write(1, &av[1][i], 1);
+            c--;
+        }
+        i++;
+    }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
