2fb3e287fc7bfc508f61684eb7156726d0ac38c7
curtyraissa <raissacurty@gmail.com>
2024-11-20 18:09:20 +0000

    feat: add search_and_replace function

--- rank2/level_1/search_and_replace.c
+++ rank2/level_1/search_and_replace.c
@@ -27,3 +27,26 @@ ZaZ eT David aiME le METal.$
 $>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
 eNcOre Un ExEmPle Pas Facile a Ecrire $
 */
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+    char l;
+
+    if (ac == 4 && !av[2][1] && !av[3][1])
+    {
+        while(av[1][i])
+        {
+            if (av[1][i] == av[2][0])
+                l = av[3][0];
+            else
+                l = av[1][i];
+            i++;
+            write(1, &l, 1);
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
