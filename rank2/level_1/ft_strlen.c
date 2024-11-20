767208bea6198117b48363163553b5dd92994d01
curtyraissa <raissacurty@gmail.com>
2024-11-20 16:54:16 +0000

    feat: add strlen function

--- rank2/level_1/ft_strlen.c
+++ rank2/level_1/ft_strlen.c
@@ -10,3 +10,13 @@ Your function must be declared as follows:
 
 int	ft_strlen(char *str);
 */
+
+int	ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
