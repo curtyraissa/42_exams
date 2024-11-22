de0b8033e337438a6708098fedac194712635438
curtyraissa <raissacurty@gmail.com>
2024-11-22 12:34:54 +0000

    feat: add atoi function

--- rank2/level_2/ft_atoi.c
+++ rank2/level_2/ft_atoi.c
@@ -13,3 +13,24 @@ Your function must be declared as follows:
 
 int	ft_atoi(const char *str);
 */
+
+int	ft_atoi(const char *str)
+{
+    int sign = 1;
+    int result = 0;
+
+    while (*str == 32 || (*str >= 9 && *str <= 13))
+        str++;
+    if (*str == '-' || *str == '+')
+    {
+        if(*str == '-')
+            sign = -1;
+        str++;
+    }
+    while (*str >= '0' && *str <= '9')
+    {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    return (result * sign);
+}
