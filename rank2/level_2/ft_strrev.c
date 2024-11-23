41ddb54ed342199c44e58c3c0e38b0c3050df7b6
curtyraissa <raissacurty@gmail.com>
2024-11-23 08:26:04 +0000

    feat: add strrev function

--- rank2/level_2/ft_strrev.c
+++ rank2/level_2/ft_strrev.c
@@ -13,3 +13,23 @@ Your function must be declared as follows:
 char    *ft_strrev(char *str);
 
 */
+
+char    *ft_strrev(char *str)
+{
+    int i = 0;
+    int len = 0;
+    char temp;
+
+    while (str[len])
+        len++;
+    len--;    
+    while (i < len)
+    {
+        temp = str[i];
+        str[i] = str[len];
+        str[len] = temp;
+        i++;
+        len--;
+    }
+    return (str);
+}
