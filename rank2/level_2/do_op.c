ab7b900050d08b9efe11cbd2f995073ceaefe170
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-11-23 16:31:21 +0000

    Update do_op.c

--- rank2/level_2/do_op.c
+++ rank2/level_2/do_op.c
@@ -28,3 +28,25 @@ $> ./do_op "1" "+" "-43" | cat -e
 $> ./do_op | cat -e
 $
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+  if (ac == 4)
+  {
+    if (av[2][0] == '+')
+      printf("%d", atoi(av[1]) + atoi(av[3]));
+    else if (av[2][0] == '-')
+      printf("%d", atoi(av[1]) - atoi(av[3]));
+    else if (av[2][0] == '*')
+      printf("%d", atoi(av[1]) * atoi(av[3]));
+    else if (av[2][i] == '/')
+      printf("%d", atoi(av[1]) / atoi(av[3]));
+    else if (av[2][i] == '%')
+      printf("%d", atoi(av[1]) % atoi(av[3]));
+  }
+  printf("\n");
+  return (0);
+}
