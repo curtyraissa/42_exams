f669ff71314fa848b8ed81210abea204049c56bd
curtyraissa <raissacurty@gmail.com>
2024-11-23 08:26:18 +0000

    feat: add last_word function

--- rank2/level_2/last_word.c
+++ rank2/level_2/last_word.c
@@ -25,3 +25,31 @@ $> ./last_word "  lorem,ipsum  " | cat -e
 lorem,ipsum$
 $>
 */
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+    int end = 0;
+    int start = 0;
+
+    if (ac == 2)
+    {
+        while (av[1][i])
+            i++;
+        while (i > 0 && av[1][i-1] == ' ' || (av[1][i-1] >= '\t' && av[1][i-1] <= '\r'))
+            i--;
+        end = i;
+       while (i > 0 && !(av[1][i-1] == ' ' || (av[1][i-1] >= '\t' && av[1][i-1] <= '\r')))
+            i--;
+       start = i;
+       while (start < end)
+        {
+            write(1, &av[1][start], 1);
+            start++;
+        }
+    }
+    write (1, "\n", 1);
+    return (0);
+}
