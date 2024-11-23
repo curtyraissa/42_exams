51a495424499b6e5e333aec179739283145b6509
curtyraissa <raissacurty@gmail.com>
2024-11-23 08:25:53 +0000

    feat: add strdup function

--- rank2/level_2/ft_strdup.c
+++ rank2/level_2/ft_strdup.c
@@ -10,3 +10,25 @@ Your function must be declared as follows:
 
 char    *ft_strdup(char *src);
 */
+
+#include <stdlib.h>
+
+char    *ft_strdup(char *src)
+{
+    int i = 0;
+    int len = 0;
+    char *dup;
+
+    while (src[len])
+        len++;
+    dup = (char *)malloc((len + 1) * sizeof(char));
+    if (!dup)
+        return (NULL);
+    while(src[i])
+    {
+        dup[i] = src[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
+}
