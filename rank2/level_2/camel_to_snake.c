7d7426a3fcf082c8be23010f15f6c1431b8096bf
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-11-23 16:25:11 +0000

    Update camel_to_snake.c

--- rank2/level_2/camel_to_snake.c
+++ rank2/level_2/camel_to_snake.c
@@ -21,3 +21,26 @@ hello_world$
 $>./camel_to_snake | cat -e
 $
 */
+
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+
+    if (ac == 2)
+    {
+        while (av[1][i])
+        {
+            if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+            {
+                av[1][i] += 32;
+                write(1, "_", 1);
+            }
+            write (1, &av[1][i]);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return(0);
+}
