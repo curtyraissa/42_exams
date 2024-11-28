e869ac35e2f99f4e12f9632b58247c91e38021dd
curtyraissa <raissacurty@gmail.com>
2024-11-28 10:01:58 +0000

    feat: add strpbrk function

--- rank2/level_2/ft_strpbrk.c
+++ rank2/level_2/ft_strpbrk.c
@@ -11,3 +11,23 @@ The function should be prototyped as follows:
 
 char	*ft_strpbrk(const char *s1, const char *s2);
 */
+
+#include <string.h>
+
+char	*ft_strpbrk(const char *s1, const char *s2)
+{
+    int i;
+
+    while (*s1)
+    {
+        i = 0;
+        while(s2[i])
+        {
+            if (s2[i] == *s1)
+                return ((char *)s1);
+            i++;
+        }
+        s1++;
+    }
+    return (NULL);
+}
