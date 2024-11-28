6dc5c42694ff41b8542c40173190a07dd21c5d0b
curtyraissa <raissacurty@gmail.com>
2024-11-28 11:25:11 +0000

    feat: add strspn function

--- rank2/level_2/ft_strspn.c
+++ rank2/level_2/ft_strspn.c
@@ -11,3 +11,29 @@ The function should be prototyped as follows:
 
 size_t	ft_strspn(const char *s, const char *accept);
 */
+
+#include <stddef.h>
+
+char *ft_strchr(const char *str, char c)
+{
+    while (*str)
+    {
+        if (*str == c)
+            return ((char *)str);
+        str++;
+    }
+    return (0);
+}
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+    int i = 0;
+
+    while (s[i])
+    {
+        if (ft_strchr(accept, s[i]) == 0)
+            break;
+        i++;
+    }
+    return (i);
+}
