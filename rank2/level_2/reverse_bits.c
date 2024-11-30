a6e56f7869276e6d107f7d5cc9a85cdba79f441c
curtyraissa <raissacurty@gmail.com>
2024-11-30 11:22:30 +0000

    Update reverse_bits

--- rank2/level_2/reverse_bits.c
+++ rank2/level_2/reverse_bits.c
@@ -24,11 +24,11 @@ _____________
 unsigned char	reverse_bits(unsigned char octet)
 {
 	int i = 8;
-	unsigned char result;
+	unsigned char result = 0;
 
 	while (i--)
 	{
-		result = result << 1;
+		result <<= 1;
 		result |= (octet & 1);
 		octet >>= 1;
 	}
