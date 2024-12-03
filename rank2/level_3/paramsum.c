1d09caedbfb5265d6e7be8ac57424f22843b9044
curtyraissa <raissacurty@gmail.com>
2024-12-03 18:50:14 +0000

    add paramsum function

--- rank2/level_3/paramsum.c
+++ rank2/level_3/paramsum.c
@@ -19,3 +19,24 @@ $>./paramsum | cat -e
 0$
 $>
 */
+
+#include <unistd.h>
+
+void ft_putnbr(int n)
+{
+    long num = n;
+    char c;
+
+    if (num > 9)
+        ft_putnbr(num / 10);
+    c = (num % 10) + '0';
+    write(1, &c, 1);
+}
+
+int main (int ac, char **av)
+{
+    (void)av;
+    ft_putnbr(ac - 1);
+    write(1, "\n", 1);
+    return (0);
+}
