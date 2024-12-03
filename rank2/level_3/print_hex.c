014035ba7721f58e1f1f29703c3e4df97127c8ba
curtyraissa <raissacurty@gmail.com>
2024-12-03 17:22:13 +0000

    add print_hex function

--- rank2/level_3/print_hex.c
+++ rank2/level_3/print_hex.c
@@ -20,3 +20,39 @@ $> ./print_hex "5156454" | cat -e
 $> ./print_hex | cat -e
 $
 */
+
+#include <unistd.h>
+
+int ft_atoi (const char *str)
+{
+    int i = 0;
+    int result = 0;
+
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return (result);
+}
+
+void print_hex(int num)
+{
+    char *hex = "0123456789abcdef";
+
+    if (num >= 16)
+        print_hex(num / 16);
+    write(1, &hex[num % 16], 1);
+}
+
+int main (int ac, char **av)
+{
+    if (ac == 2)
+    {
+        int num = ft_atoi(av[1]);
+        print_hex(num);
+
+    }
+    write(1, "\n", 1);
+    return (0);
+}
