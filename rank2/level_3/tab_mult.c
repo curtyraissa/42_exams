1d6b0f97f5d1d80909c387e9824c0911c090c31c
curtyraissa <raissacurty@gmail.com>
2024-12-03 17:22:01 +0000

    add tab_ mult function

--- rank2/level_3/tab_mult.c
+++ rank2/level_3/tab_mult.c
@@ -38,3 +38,53 @@ $>./tab_mult | cat -e
 $
 $>
 */
+
+#include <unistd.h>
+
+void ft_putnbr(int n)
+{
+    long num = n;
+    char c;
+
+    if (num > 9)
+        ft_putnbr(num / 10);
+    c = num % 10 + '0';
+    write(1, &c, 1);
+}
+
+ int ft_atoi(const char *str)
+ {
+    int result = 0;
+    int i = 0;
+
+    while (str[i] >= '0'&& str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return(result);
+ }
+
+ int main (int ac, char **av)
+ {
+    if (ac == 2)
+    {
+        int i = 1;
+        int n = ft_atoi(av[1]);
+
+        while(i <= 9)
+        {
+            ft_putnbr(i);
+            write(1, " x ", 3);
+            ft_putnbr(n);
+            write(1, " = ", 3);
+            ft_putnbr(i * n);
+            write(1, "\n", 1);
+            i++;
+        }
+
+    }
+    else
+        write(1, "\n", 1);
+    return(0);
+ }
