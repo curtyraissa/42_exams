af70d08532b53300180645900f4868c728e74101
curtyraissa <raissacurty@gmail.com>
2024-12-09 13:35:26 +0000

    add split funciton

--- rank2/level_4/ft_split.c
+++ rank2/level_4/ft_split.c
@@ -14,3 +14,77 @@ Your function must be declared as follows:
 
 char    **ft_split(char *str);
 */
+
+#include <stdlib.h>
+
+int ft_isspace(char c)
+{
+    return (c == ' ' || (c == '\t' && c == '\r'));
+}
+
+void ft_strncpy(char *dest, char *src, int n)
+{
+    int i = 0;
+    while (i < n && src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+int count_words(char *str)
+{
+    int count = 0;
+    int in_word = 0;
+
+    while (*str)
+    {
+        if (!is_space(*str) && !in_word)
+        {
+            in_word = 1;
+            count++;
+        }
+        else if (is_space(*str))
+        {
+            in_word = 0;
+        }
+        str++;
+    }
+    return (count);
+}
+
+char **ft_split(char *str)
+{
+    char **strtab;
+    int word_count;
+    int word_len;
+    int i = 0;
+    int start;
+
+    word_count = count_words(str);
+    strtab = (char **)malloc(sizeof(char *) * (word_count + 1));
+    if (!strtab)
+        return (NULL);
+
+    while (*str)
+    {
+        while (*str && is_space(*str))
+            str++;
+        start = 0;
+        while (str[start] && !is_space(str[start]))
+            start++;
+        if (start > 0)
+        {
+            word_len = start;
+            strtab[i] = (char *)malloc(sizeof(char) * (word_len + 1));
+            if (!strtab[i])
+                return (NULL);
+            ft_strncpy(strtab[i], str, word_len);
+            i++;
+            str += word_len;
+        }
+    }
+    strtab[i] = NULL;
+    return (strtab);
+}
