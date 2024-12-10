e33ef742f064fafa93c422cac6e0f4de4b583b90
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-12-10 11:28:05 +0000

    Update sort_int_tab.c

--- rank2/level_4/sort_int_tab.c
+++ rank2/level_4/sort_int_tab.c
@@ -15,3 +15,24 @@ Doubles must be preserved.
 
 Input is always coherent.
 */
+
+void sort_int_tab(int *tab, unsigned int size)
+{
+   unsigned int i = 0;
+   unsigned int len = size -1;
+   int temp;
+
+   while (i < len)
+   {
+    if (tab[i] > tab[i + 1])
+    {
+        temp = tab[i];
+        tab[i] = tab[i + 1];
+        tab[i + 1] = temp;
+        i = 0;
+    }
+    else
+        i++;
+   }
+
+}
