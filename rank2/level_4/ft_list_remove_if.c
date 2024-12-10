1b767cb3dacd9f8f91aa759c21d70948e697c100
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-12-10 12:44:01 +0000

    Update ft_list_remove_if.c

--- rank2/level_4/ft_list_remove_if.c
+++ rank2/level_4/ft_list_remove_if.c
@@ -23,3 +23,24 @@ typedef struct      s_list
 }                   t_list;
 $>
 */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+   t_list *current;
+
+   if (!begin_list || !*begin_list)
+    return;
+    
+   if (!cmp((*begin_list)->data, data_ref))
+   {
+    current = *begin_list;
+    *begin_list = (*begin_list)->next;
+    free(current);
+    ft_list_remove_if(begin_list, data_ref, cmp);
+   }
+   else
+    ft_list_remove_if(&(*begin_list)->next, data_ref, cmp);
+}
