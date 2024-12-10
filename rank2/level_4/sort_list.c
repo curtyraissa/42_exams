77b73e46ae96d2703c7d74ff473539794a9c3e40
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-12-10 11:27:43 +0000

    Update sort_list.c

--- rank2/level_4/sort_list.c
+++ rank2/level_4/sort_list.c
@@ -32,3 +32,27 @@ int ascending(int a, int b)
 	return (a <= b);
 }
 */
+
+#include "list.h"
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	t_list *current = lst;
+	int temp;
+
+	if(!lst)
+		return (NULL);
+	while(current->next)
+	{
+		if(!cmp(current->data, current->next->data))
+		{
+			temp = current->data;
+			current->data = current->next->data;
+			current->next->data = temp;
+			current = lst;
+		}
+		else
+			current = current->next;
+	}
+	return(lst);
+}
