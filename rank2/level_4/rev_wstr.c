11b95c88c8f44ec2c1dfb600d7718a8dc264844f
Raissa Curty <117386154+curtyraissa@users.noreply.github.com>
2024-12-12 20:36:38 +0000

    Update rev_wstr.c

--- rank2/level_4/rev_wstr.c
+++ rank2/level_4/rev_wstr.c
@@ -30,3 +30,47 @@ $
 $>
 
 */
+
+int ft_isspace(char c)
+{
+	return ((c == ' ') || ((c >= 9) && (c <= 13)));
+}
+
+int ft_strlen(char *str)
+{
+	int len = 0;
+	while (str[len])
+		++len;
+	return (len);
+}
+
+int main(int ac, char **av)
+{
+	char	*str;
+	int		word_start;
+	int		word_end;
+	int		len;
+	int		str_idx;
+
+	if (ac == 2)
+	{
+		str = av[1];
+		len = ft_strlen(str);
+		while (len >= 0)
+		{
+			while (!str[len] || ft_isspace(str[len]))
+				--len;
+			word_end = len;
+			while (str[len] && !ft_isspace(str[len]))
+				--len;
+			word_start = (len + 1);
+			str_idx = word_start;
+			while (word_start <= word_end)
+				write(1, &str[word_start++], 1);
+			if (str_idx != 0)
+				write(1, " ", 1);
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
